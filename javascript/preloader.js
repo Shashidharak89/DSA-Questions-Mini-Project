b16089c81554a580ecf76c957120d6f895a4c27a
Shashidharak89 <shashidharak334@gmail.com>
2024-10-03 06:39:47 +0530

    facial recognition

---
+++ javascript/preloader.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+    const loadDiv = document.querySelector('.preloader-container');
+    const conDiv = document.querySelector('.main');
+    
+    setTimeout(function() {
+        loadDiv.style.display = 'none';  // Hide the loading screen
+        conDiv.style.display = 'block';  // Show the main content
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
